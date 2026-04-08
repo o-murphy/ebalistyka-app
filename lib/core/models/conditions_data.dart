o-murphy/test_flutter_app

rollback: rollback to app-wise single current conditions state

lib/core/models/conditions_data.dart
@@ -28,7 +28,7 @@ class AtmoData {
     powderTemperature: powderTemp,
   );
 
-  static AtmoData icao() {
+  factory AtmoData.icao() {
     final atmo = Atmo.icao();
     return AtmoData(
       altitude: atmo.altitude,
@@ -36,6 +36,22 @@ class AtmoData {
       temperature: atmo.temperature,
       humidity: atmo.humidity,
       powderTemp: atmo.powderTemp,
+    );
+  }
+
+  AtmoData copyWith({
+    Distance? altitude,
+    Pressure? pressure,
+    Temperature? temperature,
+    double? humidity,
+    Temperature? powderTemp,
+  }) {
+    return AtmoData(
+      altitude: altitude ?? this.altitude,
+      pressure: pressure ?? this.pressure,
+      temperature: temperature ?? this.temperature,
+      humidity: humidity ?? this.humidity,
+      powderTemp: powderTemp ?? this.powderTemp,
     );
   }
 
@@ -153,6 +169,30 @@ class Conditions {
     if (latitudeDeg != null) 'latitudeDeg': latitudeDeg,
     if (azimuthDeg != null) 'azimuthDeg': azimuthDeg,
   };
+
+  Conditions copyWith({
+    AtmoData? atmo,
+    List<WindData>? winds,
+    Angular? lookAngle,
+    double? latitudeDeg,
+    double? azimuthDeg,
+    bool? usePowderSensitivity,
+    bool? useDiffPowderTemp,
+    bool? useCoriolis,
+    Distance? distance,
+  }) {
+    return Conditions(
+      atmo: atmo ?? this.atmo,
+      winds: winds ?? this.winds,
+      lookAngle: lookAngle ?? this.lookAngle,
+      latitudeDeg: latitudeDeg ?? this.latitudeDeg,
+      azimuthDeg: azimuthDeg ?? this.azimuthDeg,
+      usePowderSensitivity: usePowderSensitivity ?? this.usePowderSensitivity,
+      useDiffPowderTemp: useDiffPowderTemp ?? this.useDiffPowderTemp,
+      useCoriolis: useCoriolis ?? this.useCoriolis,
+      targetDistance: distance ?? this.distance,
+    );
+  }
 
   static Conditions fromJson(Map<String, dynamic> json) {
     final atmo = json['atmo'] as Map;

lib/features/tables/details_table_mv.dart
@@ -1,6 +1,8 @@
+import 'package:eballistica/core/models/conditions_data.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:eballistica/core/providers/settings_provider.dart';
+import 'package:eballistica/core/providers/shot_conditions_provider.dart';
 import 'package:eballistica/core/providers/shot_profile_provider.dart';
 import 'package:eballistica/core/models/app_settings.dart';
 import 'package:eballistica/core/models/field_constraints.dart';
@@ -58,13 +60,17 @@ class DetailsTableData {
 
 // ── Private builder ──────────────────────────────────────────────────────────
 
-DetailsTableData _buildDetails(ShotProfile profile, AppSettings settings) {
+DetailsTableData _buildDetails(
+  ShotProfile profile,
+  Conditions conditions,
+  AppSettings settings,
+) {
   final units = settings.units;
   final rifle = profile.rifle;
   final cart = profile.cartridge!;
   final proj = cart.projectile;
-  final conds = profile.conditions;
-  final winds = profile.winds;
+  final atmo = conditions.atmo;
+  final winds = conditions.winds;
 
   final twistInch = rifle.twist.in_(Unit.inch);
   final weightGr = proj.weight.in_(Unit.grain);
@@ -72,9 +78,10 @@ DetailsTableData _buildDetails(ShotProfile profile, AppSettings settings) {
   final lenInch = proj.length.in_(Unit.inch);
 
   // Powder sensitivity — separate flags for zero and current
-  final currentPowderSensOn = profile.usePowderSensitivity;
+  final currentPowderSensOn = conditions.usePowderSensitivity;
   final zeroPowderSensOn = cart.usePowderSensitivity;
-  final currentUseDiffTemp = currentPowderSensOn && profile.useDiffPowderTemp;
+  final currentUseDiffTemp =
+      currentPowderSensOn && conditions.useDiffPowderTemp;
   final zeroUseDiffTemp = zeroPowderSensOn && cart.useDiffPowderTemp;
 
   final refMvMps = cart.mv.in_(Unit.mps);
@@ -88,7 +95,7 @@ DetailsTableData _buildDetails(ShotProfile profile, AppSettings settings) {
   );
 
   // Zero MV
-  final zeroAtmo = cart.conditions ?? conds;
+  final zeroAtmo = cart.conditions ?? atmo;
   final zeroPowderTempC = zeroUseDiffTemp
       ? zeroAtmo.powderTemp.in_(Unit.celsius)
       : zeroAtmo.temperature.in_(Unit.celsius);
@@ -96,12 +103,13 @@ DetailsTableData _buildDetails(ShotProfile profile, AppSettings settings) {
 
   // Current MV
   final currTempC = currentUseDiffTemp
-      ? conds.powderTemp.in_(Unit.celsius)
-      : conds.temperature.in_(Unit.celsius);
+      ? atmo.powderTemp.in_(Unit.celsius)
+      : atmo.temperature.in_(Unit.celsius);
   final currentMvMps = currentPowderSensOn ? mvAtTempC(currTempC) : refMvMps;
 
   // Gyrostability (Miller)
-  double sg = profile.toCurrentShot().calculateStabilityCoefficient();
+  final currentShot = profile.toCurrentShot(conditions);
+  double sg = currentShot.calculateStabilityCoefficient();
 
   // Sectional density + form factor
   final sd = (weightGr > 0 && diamInch > 0)
@@ -159,13 +167,13 @@ DetailsTableData _buildDetails(ShotProfile profile, AppSettings settings) {
     sectionalDensity: sd?.toStringAsFixed(3),
     gyroStability: sg.toStringAsFixed(2),
     temperature: () {
-      final t = conds.temperature.in_(units.temperature);
+      final t = atmo.temperature.in_(units.temperature);
       return '${t.toStringAsFixed(FC.temperature.accuracyFor(units.temperature))} ${units.temperature.symbol}';
     }(),
     humidity:
-        '${(conds.humidity.convert(Unit.fraction, Unit.percent)).toStringAsFixed(0)} %',
+        '${(atmo.humidity.convert(Unit.fraction, Unit.percent)).toStringAsFixed(0)} %',
     pressure: () {
-      final p = conds.pressure.in_(units.pressure);
+      final p = atmo.pressure.in_(units.pressure);
       return '${p.toStringAsFixed(FC.pressure.accuracyFor(units.pressure))} ${units.pressure.symbol}';
     }(),
     windSpeed: winds.isNotEmpty
@@ -184,9 +192,11 @@ DetailsTableData _buildDetails(ShotProfile profile, AppSettings settings) {
 
 final detailsTableMvProvider = Provider<DetailsTableData?>((ref) {
   final profile = ref.watch(shotProfileProvider).value;
+  final conditions = ref.watch(shotConditionsProvider).value;
   final settings = ref.watch(settingsProvider).value;
 
-  if (profile == null || settings == null) return null;
+  if (profile == null || conditions == null || settings == null) return null;
+  if (profile.cartridge == null) return null;
 
-  return _buildDetails(profile, settings);
+  return _buildDetails(profile, conditions, settings);
 });

lib/features/tables/trajectory_tables_vm.dart
@@ -1,3 +1,4 @@
+import 'package:eballistica/core/models/conditions_data.dart';
 import 'package:eballistica/core/models/unit_settings.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -7,10 +8,12 @@ import 'package:eballistica/core/formatting/unit_formatter.dart';
 import 'package:eballistica/core/providers/formatter_provider.dart';
 import 'package:eballistica/core/providers/service_providers.dart';
 import 'package:eballistica/core/providers/settings_provider.dart';
+import 'package:eballistica/core/providers/shot_conditions_provider.dart'; // ← додати
 import 'package:eballistica/core/providers/shot_profile_provider.dart';
 import 'package:eballistica/core/models/app_settings.dart';
 import 'package:eballistica/core/models/field_constraints.dart';
 import 'package:eballistica/core/models/shot_profile.dart';
+import 'package:eballistica/core/solver/conditions.dart'; // ← додати
 import 'package:eballistica/core/solver/trajectory_data.dart';
 import 'package:eballistica/core/solver/unit.dart';
 import 'package:eballistica/shared/models/formatted_row.dart';
@@ -53,10 +56,18 @@ class TrajectoryTablesViewModel extends AsyncNotifier<TrajectoryTablesUiState> {
 
   Future<void> recalculate() async {
     final profile = ref.read(shotProfileProvider).value;
+    final conditions = ref
+        .read(shotConditionsProvider)
+        .value; // ← додаємо conditions
     final settings = ref.read(settingsProvider).value;
     final formatter = ref.read(unitFormatterProvider);
 
-    if (profile == null || settings == null) {
+    if (profile == null || conditions == null || settings == null) {
+      state = const AsyncData(TrajectoryTablesUiEmpty());
+      return;
+    }
+
+    if (profile.cartridge == null) {
       state = const AsyncData(TrajectoryTablesUiEmpty());
       return;
     }
@@ -73,13 +84,17 @@ class TrajectoryTablesViewModel extends AsyncNotifier<TrajectoryTablesUiState> {
         stepM: cfg.stepM < 1.0 ? cfg.stepM : 1.0,
       );
 
-      final zeroKey = _buildZeroKey(profile);
+      final zeroKey = _buildZeroKey(
+        profile,
+        conditions,
+      ); // ← передаємо conditions
       final useCache = listEquals(zeroKey, _lastZeroKey);
 
       final result = await ref
           .read(ballisticsServiceProvider)
           .calculateTable(
             profile,
+            conditions, // ← передаємо conditions!
             opts,
             cachedZeroElevRad: useCache ? _cachedZeroElevRad : null,
           );
@@ -91,6 +106,7 @@ class TrajectoryTablesViewModel extends AsyncNotifier<TrajectoryTablesUiState> {
 
       final uiState = _buildReadyState(
         profile: profile,
+        conditions: conditions, // ← передаємо conditions
         settings: settings,
         formatter: formatter,
         result: result,
@@ -106,6 +122,7 @@ class TrajectoryTablesViewModel extends AsyncNotifier<TrajectoryTablesUiState> {
 
   TrajectoryTablesUiReady _buildReadyState({
     required ShotProfile profile,
+    required Conditions conditions, // ← додаємо conditions
     required AppSettings settings,
     required UnitFormatter formatter,
     required BallisticsResult result,
@@ -329,12 +346,14 @@ class TrajectoryTablesViewModel extends AsyncNotifier<TrajectoryTablesUiState> {
 
   // ── Zero key ───────────────────────────────────────────────────────────────
 
-  List<double> _buildZeroKey(ShotProfile profile) {
+  List<double> _buildZeroKey(ShotProfile profile, Conditions conditions) {
     final c = profile.cartridge!;
-    final zeroAtmo = c.conditions ?? profile.conditions;
+    // Використовуємо умови з набою або глобальні умови
+    final zeroAtmo = c.conditions ?? conditions.atmo;
     final r = profile.rifle;
     final proj = c.projectile;
     final zeroUsePowderSens = c.usePowderSensitivity;
+
     return [
       r.sightHeight.in_(Unit.meter),
       r.twist.in_(Unit.inch),
@@ -352,7 +371,7 @@ class TrajectoryTablesViewModel extends AsyncNotifier<TrajectoryTablesUiState> {
       zeroAtmo.humidity,
       zeroAtmo.powderTemp.in_(Unit.celsius),
       c.zeroDistance.in_(Unit.meter),
-      profile.lookAngle.in_(Unit.radian),
+      conditions.lookAngle.in_(Unit.radian), // ← беремо з conditions!
       zeroUsePowderSens ? 1.0 : 0.0,
       c.useDiffPowderTemp ? 1.0 : 0.0,
     ];

o-murphy/ebalistyka-app

lib/core/models/conditions_data.dart
@@ -28,7 +28,7 @@ class AtmoData {
     powderTemperature: powderTemp,
   );
 
-  static AtmoData icao() {
+  factory AtmoData.icao() {
     final atmo = Atmo.icao();
     return AtmoData(
       altitude: atmo.altitude,
@@ -36,6 +36,22 @@ class AtmoData {
       temperature: atmo.temperature,
       humidity: atmo.humidity,
       powderTemp: atmo.powderTemp,
+    );
+  }
+
+  AtmoData copyWith({
+    Distance? altitude,
+    Pressure? pressure,
+    Temperature? temperature,
+    double? humidity,
+    Temperature? powderTemp,
+  }) {
+    return AtmoData(
+      altitude: altitude ?? this.altitude,
+      pressure: pressure ?? this.pressure,
+      temperature: temperature ?? this.temperature,
+      humidity: humidity ?? this.humidity,
+      powderTemp: powderTemp ?? this.powderTemp,
     );
   }
 
@@ -128,10 +144,10 @@ class Conditions {
     this.latitudeDeg,
     this.azimuthDeg,
     AtmoData? atmo,
-    Distance? targetDistance,
+    Distance? distance,
     Angular? lookAngle,
   }) : atmo = atmo ?? AtmoData.icao(),
-       distance = targetDistance ?? Distance(100.0, Unit.meter),
+       distance = distance ?? Distance(100.0, Unit.meter),
        lookAngle = lookAngle ?? Angular(0.0, Unit.radian);
 
   Atmo toAtmo() => Atmo(
@@ -154,6 +170,30 @@ class Conditions {
     if (azimuthDeg != null) 'azimuthDeg': azimuthDeg,
   };
 
+  Conditions copyWith({
+    AtmoData? atmo,
+    List<WindData>? winds,
+    Angular? lookAngle,
+    double? latitudeDeg,
+    double? azimuthDeg,
+    bool? usePowderSensitivity,
+    bool? useDiffPowderTemp,
+    bool? useCoriolis,
+    Distance? distance,
+  }) {
+    return Conditions(
+      atmo: atmo ?? this.atmo,
+      winds: winds ?? this.winds,
+      lookAngle: lookAngle ?? this.lookAngle,
+      latitudeDeg: latitudeDeg ?? this.latitudeDeg,
+      azimuthDeg: azimuthDeg ?? this.azimuthDeg,
+      usePowderSensitivity: usePowderSensitivity ?? this.usePowderSensitivity,
+      useDiffPowderTemp: useDiffPowderTemp ?? this.useDiffPowderTemp,
+      useCoriolis: useCoriolis ?? this.useCoriolis,
+      distance: distance ?? this.distance,
+    );
+  }
+
   static Conditions fromJson(Map<String, dynamic> json) {
     final atmo = json['atmo'] as Map;
 
@@ -168,7 +208,7 @@ class Conditions {
       ),
       latitudeDeg: (json['latitudeDeg'] as num?)?.toDouble(),
       azimuthDeg: (json['azimuthDeg'] as num?)?.toDouble(),
-      targetDistance: json['targetDistance'] != null
+      distance: json['targetDistance'] != null
           ? Distance(
               (json['targetDistance'] as num).toDouble(),
               StorageUnits.profileTargetDistance,

lib/features/tables/details_table_mv.dart
@@ -1,6 +1,8 @@
+import 'package:eballistica/core/models/conditions_data.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:eballistica/core/providers/settings_provider.dart';
+import 'package:eballistica/core/providers/shot_conditions_provider.dart';
 import 'package:eballistica/core/providers/shot_profile_provider.dart';
 import 'package:eballistica/core/models/app_settings.dart';
 import 'package:eballistica/core/models/field_constraints.dart';
@@ -58,13 +60,17 @@ class DetailsTableData {
 
 // ── Private builder ──────────────────────────────────────────────────────────
 
-DetailsTableData _buildDetails(ShotProfile profile, AppSettings settings) {
+DetailsTableData _buildDetails(
+  ShotProfile profile,
+  Conditions conditions,
+  AppSettings settings,
+) {
   final units = settings.units;
   final rifle = profile.rifle;
   final cart = profile.cartridge!;
   final proj = cart.projectile;
-  final conds = profile.conditions;
-  final winds = profile.winds;
+  final atmo = conditions.atmo;
+  final winds = conditions.winds;
 
   final twistInch = rifle.twist.in_(Unit.inch);
   final weightGr = proj.weight.in_(Unit.grain);
@@ -72,9 +78,10 @@ DetailsTableData _buildDetails(ShotProfile profile, AppSettings settings) {
   final lenInch = proj.length.in_(Unit.inch);
 
   // Powder sensitivity — separate flags for zero and current
-  final currentPowderSensOn = profile.usePowderSensitivity;
+  final currentPowderSensOn = conditions.usePowderSensitivity;
   final zeroPowderSensOn = cart.usePowderSensitivity;
-  final currentUseDiffTemp = currentPowderSensOn && profile.useDiffPowderTemp;
+  final currentUseDiffTemp =
+      currentPowderSensOn && conditions.useDiffPowderTemp;
   final zeroUseDiffTemp = zeroPowderSensOn && cart.useDiffPowderTemp;
 
   final refMvMps = cart.mv.in_(Unit.mps);
@@ -88,7 +95,7 @@ DetailsTableData _buildDetails(ShotProfile profile, AppSettings settings) {
   );
 
   // Zero MV
-  final zeroAtmo = cart.conditions ?? conds;
+  final zeroAtmo = cart.atmo ?? atmo;
   final zeroPowderTempC = zeroUseDiffTemp
       ? zeroAtmo.powderTemp.in_(Unit.celsius)
       : zeroAtmo.temperature.in_(Unit.celsius);
@@ -96,12 +103,13 @@ DetailsTableData _buildDetails(ShotProfile profile, AppSettings settings) {
 
   // Current MV
   final currTempC = currentUseDiffTemp
-      ? conds.powderTemp.in_(Unit.celsius)
-      : conds.temperature.in_(Unit.celsius);
+      ? atmo.powderTemp.in_(Unit.celsius)
+      : atmo.temperature.in_(Unit.celsius);
   final currentMvMps = currentPowderSensOn ? mvAtTempC(currTempC) : refMvMps;
 
   // Gyrostability (Miller)
-  double sg = profile.toCurrentShot().calculateStabilityCoefficient();
+  final currentShot = profile.toCurrentShot(conditions);
+  double sg = currentShot.calculateStabilityCoefficient();
 
   // Sectional density + form factor
   final sd = (weightGr > 0 && diamInch > 0)
@@ -159,13 +167,13 @@ DetailsTableData _buildDetails(ShotProfile profile, AppSettings settings) {
     sectionalDensity: sd?.toStringAsFixed(3),
     gyroStability: sg.toStringAsFixed(2),
     temperature: () {
-      final t = conds.temperature.in_(units.temperature);
+      final t = atmo.temperature.in_(units.temperature);
       return '${t.toStringAsFixed(FC.temperature.accuracyFor(units.temperature))} ${units.temperature.symbol}';
     }(),
     humidity:
-        '${(conds.humidity.convert(Unit.fraction, Unit.percent)).toStringAsFixed(0)} %',
+        '${(atmo.humidity.convert(Unit.fraction, Unit.percent)).toStringAsFixed(0)} %',
     pressure: () {
-      final p = conds.pressure.in_(units.pressure);
+      final p = atmo.pressure.in_(units.pressure);
       return '${p.toStringAsFixed(FC.pressure.accuracyFor(units.pressure))} ${units.pressure.symbol}';
     }(),
     windSpeed: winds.isNotEmpty
@@ -184,9 +192,11 @@ DetailsTableData _buildDetails(ShotProfile profile, AppSettings settings) {
 
 final detailsTableMvProvider = Provider<DetailsTableData?>((ref) {
   final profile = ref.watch(shotProfileProvider).value;
+  final conditions = ref.watch(shotConditionsProvider).value;
   final settings = ref.watch(settingsProvider).value;
 
-  if (profile == null || settings == null) return null;
+  if (profile == null || conditions == null || settings == null) return null;
+  if (profile.cartridge == null) return null;
 
-  return _buildDetails(profile, settings);
+  return _buildDetails(profile, conditions, settings);
 });

lib/features/tables/trajectory_tables_vm.dart
@@ -1,3 +1,4 @@
+import 'package:eballistica/core/models/conditions_data.dart';
 import 'package:eballistica/core/models/unit_settings.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -7,10 +8,12 @@ import 'package:eballistica/core/formatting/unit_formatter.dart';
 import 'package:eballistica/core/providers/formatter_provider.dart';
 import 'package:eballistica/core/providers/service_providers.dart';
 import 'package:eballistica/core/providers/settings_provider.dart';
+import 'package:eballistica/core/providers/shot_conditions_provider.dart'; // ← додати
 import 'package:eballistica/core/providers/shot_profile_provider.dart';
 import 'package:eballistica/core/models/app_settings.dart';
 import 'package:eballistica/core/models/field_constraints.dart';
 import 'package:eballistica/core/models/shot_profile.dart';
+import 'package:eballistica/core/solver/conditions.dart'; // ← додати
 import 'package:eballistica/core/solver/trajectory_data.dart';
 import 'package:eballistica/core/solver/unit.dart';
 import 'package:eballistica/shared/models/formatted_row.dart';
@@ -53,10 +56,18 @@ class TrajectoryTablesViewModel extends AsyncNotifier<TrajectoryTablesUiState> {
 
   Future<void> recalculate() async {
     final profile = ref.read(shotProfileProvider).value;
+    final conditions = ref
+        .read(shotConditionsProvider)
+        .value; // ← додаємо conditions
     final settings = ref.read(settingsProvider).value;
     final formatter = ref.read(unitFormatterProvider);
 
-    if (profile == null || settings == null) {
+    if (profile == null || conditions == null || settings == null) {
+      state = const AsyncData(TrajectoryTablesUiEmpty());
+      return;
+    }
+
+    if (profile.cartridge == null) {
       state = const AsyncData(TrajectoryTablesUiEmpty());
       return;
     }
@@ -73,13 +84,17 @@ class TrajectoryTablesViewModel extends AsyncNotifier<TrajectoryTablesUiState> {
         stepM: cfg.stepM < 1.0 ? cfg.stepM : 1.0,
       );
 
-      final zeroKey = _buildZeroKey(profile);
+      final zeroKey = _buildZeroKey(
+        profile,
+        conditions,
+      ); // ← передаємо conditions
       final useCache = listEquals(zeroKey, _lastZeroKey);
 
       final result = await ref
           .read(ballisticsServiceProvider)
           .calculateTable(
             profile,
+            conditions, // ← передаємо conditions!
             opts,
             cachedZeroElevRad: useCache ? _cachedZeroElevRad : null,
           );
@@ -91,6 +106,7 @@ class TrajectoryTablesViewModel extends AsyncNotifier<TrajectoryTablesUiState> {
 
       final uiState = _buildReadyState(
         profile: profile,
+        conditions: conditions, // ← передаємо conditions
         settings: settings,
         formatter: formatter,
         result: result,
@@ -106,6 +122,7 @@ class TrajectoryTablesViewModel extends AsyncNotifier<TrajectoryTablesUiState> {
 
   TrajectoryTablesUiReady _buildReadyState({
     required ShotProfile profile,
+    required Conditions conditions, // ← додаємо conditions
     required AppSettings settings,
     required UnitFormatter formatter,
     required BallisticsResult result,
@@ -329,12 +346,14 @@ class TrajectoryTablesViewModel extends AsyncNotifier<TrajectoryTablesUiState> {
 
   // ── Zero key ───────────────────────────────────────────────────────────────
 
-  List<double> _buildZeroKey(ShotProfile profile) {
+  List<double> _buildZeroKey(ShotProfile profile, Conditions conditions) {
     final c = profile.cartridge!;
-    final zeroAtmo = c.conditions ?? profile.conditions;
+    // Використовуємо умови з набою або глобальні умови
+    final zeroAtmo = c.atmo ?? conditions.atmo;
     final r = profile.rifle;
     final proj = c.projectile;
     final zeroUsePowderSens = c.usePowderSensitivity;
+
     return [
       r.sightHeight.in_(Unit.meter),
       r.twist.in_(Unit.inch),
@@ -352,7 +371,7 @@ class TrajectoryTablesViewModel extends AsyncNotifier<TrajectoryTablesUiState> {
       zeroAtmo.humidity,
       zeroAtmo.powderTemp.in_(Unit.celsius),
       c.zeroDistance.in_(Unit.meter),
-      profile.lookAngle.in_(Unit.radian),
+      conditions.lookAngle.in_(Unit.radian), // ← беремо з conditions!
       zeroUsePowderSens ? 1.0 : 0.0,
       c.useDiffPowderTemp ? 1.0 : 0.0,
     ];