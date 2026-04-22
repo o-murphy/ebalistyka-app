bc77fdf33966abe860a22a8ea5511d5f4c286592
o-murphy <thehelixpg@gmail.com>
2026-04-22 15:28:21 +0300

    update: workflows

---
+++ scripts/setup-linux-deps.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+echo "--- Apdate package list... ---"
+sudo apt-get update -q
+
+echo "--- Setup build dependencies (Linux)... ---"
+sudo apt-get install -y \
+  clang \
+  cmake \
+  ninja-build \
+  pkg-config \
+  libgtk-3-dev \
+  liblzma-dev \
+  libstdc++-12-dev \
+  libclang-dev \
+  fuse \
+  libfuse2
+
+echo "--- Setup complete success! ---"
