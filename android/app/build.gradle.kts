88c61b45e12339027df8a4ac03db88f7cda6e5b8
o-murphy <thehelixpg@gmail.com>
2026-04-24 01:40:22 +0300

    seems: running on android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.o.murphy.ebalistyka"
+    namespace = "com.example.ebalistyka"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.o.murphy.ebalistyka"
+        applicationId = "com.example.ebalistyka"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/ebalistyka/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/ebalistyka/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.test_app
+package com.example.ebalistyka
 
 import io.flutter.embedding.android.FlutterActivity
 
